baae1f8e4568e40c81e686df33085fa4c68a036a
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-07-26 13:02:19 +0300

    adding the search component

---
+++ src/components/ui/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../../img/logo.png' //can  import since react uses webpack
+//making the header a flex box centering everything
+
+const Header = () => {
+  return (
+    <header className='center'>  
+    <img src= {logo} alt='' />
+ 
+    </header>
+  )
+}
+
+export default Header
